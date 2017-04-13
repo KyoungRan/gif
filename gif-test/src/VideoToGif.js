40b0b69e81852ec905efa4b16c5f86808cce80cb
KyoungRan <kyoungrankang@gmail.com>
2017-04-13 15:33:02 +0900

    just saved

--- gif-test/src/VideoToGif.js
+++ gif-test/src/VideoToGif.js
@@ -8,8 +8,12 @@ class VideoToGif extends Component {
     this.state = {
       videoUrl: ""
     };
-    this.createVideoToGif = this.createVideoToGif.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.createVideoToGif = this
+      .createVideoToGif
+      .bind(this);
+    this.handleChange = this
+      .handleChange
+      .bind(this);
   }
 
   createVideoToGif(e) {
@@ -17,53 +21,48 @@ class VideoToGif extends Component {
     const videoUrl = this.state.videoUrl;
     console.log(videoUrl);
 
-    gifshot.createGIF(
-      {
-        video: videoUrl
-      },
-      function(obj) {
-        if (!obj.error) {
-          const image = obj.image,
-            //var image = obj.image, animatedImage = document.createElement("img");
-            //animatedImage.src = image;
-            gifResult = document.getElementById("video-result");
-          gifResult.src = image;
-          //document.body.appendChild(animatedImage);
-        }
+    gifshot.createGIF({
+      video: videoUrl
+    }, function (obj) {
+      if (!obj.error) {
+        const image = obj.image,
+          // var image = obj.image, animatedImage = document.createElement("img");
+          // animatedImage.src = image;
+          gifResult = document.getElementById("video-result");
+        gifResult.src = image;
+        //document.body.appendChild(animatedImage);
       }
-    );
+    });
   }
 
   handleChange(e) {
-    this.setState({
-      videoUrl: e.target.value
-    });
+    this.setState({videoUrl: e.target.value});
   }
 
   render() {
     return (
       <div>
-        <br />
-        <hr />
+        <br/>
+        <hr/>
         <form onSubmit={this.createVideoToGif}>
-          <p> Youtube Video URL을 입력하세요. </p>
+          <p>
+            Youtube Video URL을 입력하세요.
+          </p>
           <input
             className="video-input"
             type="text"
             placeholder="Youtube Video URL"
             value={this.state.videoUrl}
             onChange={this.handleChange}
-            required
-          />
-          {/*<button className="button" type="submit" value="Submit">Submit</button>*/}
-          <br /><br />
+            required/> {/*<button className="button" type="submit" value="Submit">Submit</button>*/}
+          <br/><br/>
           <button className="button" type="submit">
             Create GIF
           </button>
-          <br />
+          <br/>
         </form>
         <div className="gif-container">
-          <img id="video-result" className="image-gif-preview" />
+          <img id="video-result" className="image-gif-preview"/>
         </div>
       </div>
     );
